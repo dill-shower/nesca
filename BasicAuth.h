d75cea89d77e766392c06a263a4426c44ee2305a
dill-shower <114938733+dill-shower@users.noreply.github.com>
2024-10-19 20:21:18 +0000

    Update BasicAuth.h

--- BasicAuth.h
+++ BasicAuth.h
@@ -6,14 +6,15 @@
 #include "Connector.h"
 #include "externData.h"
 #include "mainResources.h"
+#include <string>
 
 class BA {
 private:
-	static lopaStr BABrute(const char *ip, const int port, bool performDoubleCheck);
+    static lopaStr BABrute(const char *ip, int port, bool performDoubleCheck);
 
 public:
-	static int checkOutput(const string *buffer, const char *ip, const int port);
-	static lopaStr BALobby(const char *ip, const int port, bool performDoubleCheck);
+    static int checkOutput(const std::string& buffer, const char *ip, int port);
+    static lopaStr BALobby(const std::string& ip, int port, bool performDoubleCheck);
 };
 
 #endif // BASICAUTH_H
