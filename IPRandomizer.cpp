e8761cb237b40661151683a7a0736ca744941f33
dill-shower <114938733+dill-shower@users.noreply.github.com>
2024-10-19 20:31:25 +0000

    Update IPRandomizer.cpp

--- IPRandomizer.cpp
+++ IPRandomizer.cpp
@@ -1,53 +1,71 @@
 #include "IPRandomizer.h"
 
-
 IPRandomizer::IPRandomizer(std::vector<IPRangeHolder> ipRangeVec, int shuffleGap)
 {
-	this->ipRangeVec = ipRangeVec;
-	this->shuffleGap = shuffleGap;
+    this->ipRangeVec = ipRangeVec;
+    this->shuffleGap = shuffleGap;
 
-	for (int i = 0; i < ipRangeVec.size(); ++i) {
-		this->shuffleOffset.push_back(0);
-	}
+    for (int i = 0; i < ipRangeVec.size(); ++i) {
+        this->shuffleOffset.push_back(0);
+    }
 }
 
+IPRandomizer::IPRandomizer(std::vector<IPRangeHolder> ipRangeVec) : IPRandomizer(ipRangeVec, 20000) {}
 
 IPRandomizer::~IPRandomizer()
 {
-	this->ipRangeVec.clear();
-	this->shuffleOffset.clear();
+    this->ipRangeVec.clear();
+    this->shuffleOffset.clear();
 }
 
 void IPRandomizer::shuffleRange() {
-	for (int i = 0; i < this->ipRangeVec.size(); ++i) {
-		IPRangeHolder ipRangeHolder = this->ipRangeVec[i];
-		if (ipRangeHolder.ip1 + this->shuffleOffset[i] >= ipRangeHolder.ip2) {
-			continue;
-		}
+    std::random_device rd;
+    std::mt19937 rng(rd());
 
-		unsigned int rangeSize = ipRangeHolder.ip2 - (ipRangeHolder.ip1 + this->shuffleOffset[i] - 1);
-		int offset = (rangeSize < this->shuffleGap ? rangeSize : this->shuffleGap);
+    for (int i = 0; i < this->ipRangeVec.size(); ++i) {
+        IPRangeHolder ipRangeHolder = this->ipRangeVec[i];
+        if (ipRangeHolder.ip1 + this->shuffleOffset[i] >= ipRangeHolder.ip2) {
+            continue;
+        }
 
-		for (unsigned int j = this->shuffleOffset[i]; j < this->shuffleOffset[i] + offset; ++j) {
-			this->shuffledRange.push_back(ipRangeHolder.ip1 + j);
-		}
+        unsigned int rangeSize = ipRangeHolder.ip2 - (ipRangeHolder.ip1 + this->shuffleOffset[i] - 1);
+        int offset = (rangeSize < this->shuffleGap ? rangeSize : this->shuffleGap);
 
-		this->shuffleOffset[i] += offset;
-	}
-	std::random_shuffle(this->shuffledRange.begin(), this->shuffledRange.end());
+        for (unsigned int j = this->shuffleOffset[i]; j < this->shuffleOffset[i] + offset; ++j) {
+            this->shuffledRange.push_back(ipRangeHolder.ip1 + j);
+        }
+
+        this->shuffleOffset[i] += offset;
+    }
+    std::shuffle(this->shuffledRange.begin(), this->shuffledRange.end(), rng);
 }
 
 unsigned int IPRandomizer::getNext() {
-	if (this->shuffledRange.empty()) {
-		shuffleRange();
+    if (this->shuffledRange.empty()) {
+        IPRangeHolder ipRangeHolder = this->ipRangeVec[this->currentRangeIndex];
+        if (ipRangeHolder.ip1 + this->shuffleOffset[this->currentRangeIndex] >= ipRangeHolder.ip2) {
+            this->currentRangeIndex++;
+            if (this->currentRangeIndex >= this->ipRangeVec.size()) {
+                this->currentRangeIndex = 0;
+                std::fill(this->shuffleOffset.begin(), this->shuffleOffset.end(), 0);
+            }
+            ipRangeHolder = this->ipRangeVec[this->currentRangeIndex];
+        }
 
-		//If still empty then ip-range chunk is depleted.
-		if (this->shuffledRange.empty()) {
-			return 0;
-		};
-	};
+        unsigned int rangeSize = ipRangeHolder.ip2 - (ipRangeHolder.ip1 + this->shuffleOffset[this->currentRangeIndex] - 1);
+        int offset = (rangeSize < this->shuffleGap ? rangeSize : this->shuffleGap);
 
-	unsigned int ip = this->shuffledRange[0];
-	this->shuffledRange.erase(this->shuffledRange.begin());
-	return ip;
+        for (unsigned int j = this->shuffleOffset[this->currentRangeIndex]; j < this->shuffleOffset[this->currentRangeIndex] + offset; ++j) {
+            this->shuffledRange.push_back(ipRangeHolder.ip1 + j);
+        }
+
+        this->shuffleOffset[this->currentRangeIndex] += offset;
+        std::random_device rd;
+        std::mt19937 rng(rd());
+        std::shuffle(this->shuffledRange.begin(), this->shuffledRange.end(), rng);
+    }
+
+    unsigned int ip = this->shuffledRange.front();
+    this->shuffledRange.pop_front();
+    return ip;
 }
